a7a95e90fcf591e8f3f7270b2a350f5017fb0a17
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-15 17:21:33 +0500

    first commit

--- dbConnect.php
+++ dbConnect.php
@@ -1,9 +1,14 @@
 <?php  
 // Connect with the database  
-$db = new mysqli(localhost, root, root, paypal_sub);  
+$db = new mysqli(eu-cdbr-west-02, b770ae34b12c19, b5c5c63a, eu-cdbr-west-02);  
   
 // Display error if failed to connect  
 if ($db->connect_errno) {  
     printf("Connect failed: %s\n", $db->connect_error);  
     exit();  
 }
+
+define('DB_HOST', 'eu-cdbr-west-02.cleardb.net');  
+define('DB_USERNAME', 'b770ae34b12c19');  
+define('DB_PASSWORD', 'b5c5c63a');  
+define('DB_NAME', 'eu-cdbr-west-02.cleardb.net');  
