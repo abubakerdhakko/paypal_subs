ee1f381f9f1662f4d04ebdd377b8e08e48628346
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-15 19:11:52 +0500

    first commit

--- dbConnect.php
+++ dbConnect.php
@@ -1,18 +1,16 @@
 <?php  
+// Connect with the database  
 $url = parse_url(getenv("mysql://b770ae34b12c19:b5c5c63a@eu-cdbr-west-02.cleardb.net/heroku_0885a564f4e05ce?reconnect=true "));
 $server = $url["eu-cdbr-west-02.cleardb.net"];
 $username = $url["b770ae34b12c19"];
 $password = $url["b5c5c63a"];
 $db = substr($url["heroku_0885a564f4e05ce"], 1);
 $connection = new mysqli($server, $username, $password, $db);
-// Connect with the database  
 // $db = new mysqli(eu-cdbr-west-02.cleardb.net, b5c5c63a, b770ae34b12c19,  heroku_0885a564f4e05ce);  
   
 // Display error if failed to connect  
 if ($connection->connect_errno) {  
-    printf("Connectionnnnnnn: failed: %s\n", $db->connect_error);  
+    printf("Connect failed: %s\n", $connection->connect_error);  
 
-
-    
     exit();  
 }
