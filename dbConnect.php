b234256868f3953069f0ea5b8a5159083c899c29
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-15 17:43:51 +0500

    first commit

--- dbConnect.php
+++ dbConnect.php
@@ -5,5 +5,7 @@ $db = new mysqli(eu-cdbr-west-02, b770ae34b12c19, b5c5c63a, eu-cdbr-west-02);
 // Display error if failed to connect  
 if ($db->connect_errno) {  
     printf("Connect failed: %s\n", $db->connect_error);  
+
+    
     exit();  
 }
