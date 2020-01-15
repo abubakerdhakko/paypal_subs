24349e0c4e66165e07889935b10bd6c934ff6c2b
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-15 17:05:27 +0500

    first commit

--- config.php
+++ config.php
@@ -10,16 +10,16 @@ $itemPrice = 25.00;
 define('PAYPAL_ID', 'sb-uvmyk858514@business.example.com');  
 define('PAYPAL_SANDBOX', TRUE); //TRUE or FALSE  
   
-define('PAYPAL_RETURN_URL', 'http://localhost:8888/paypal-subs/success.php');  
-define('PAYPAL_CANCEL_URL', 'http://localhost:8888/paypal-subs/cancel.php');  
-define('PAYPAL_NOTIFY_URL', 'http://localhost:8888/paypal_subs/paypal_ipn.php');  
+define('PAYPAL_RETURN_URL', 'http://bliss-zone.herokuapp.com/paypal-subs/success.php');  
+define('PAYPAL_CANCEL_URL', 'http://bliss-zone.herokuapp.com/paypal-subs/cancel.php');  
+define('PAYPAL_NOTIFY_URL', 'http://bliss-zone.herokuapp.com/paypal_subs/paypal_ipn.php');  
 define('PAYPAL_CURRENCY', 'USD');  
   
 // Database configuration  
-define('DB_HOST', 'localhost');  
-define('DB_USERNAME', 'root');  
-define('DB_PASSWORD', 'root');  
-define('DB_NAME', 'paypal_sub');  
+define('DB_HOST', 'eu-cdbr-west-02.cleardb.net');  
+define('DB_USERNAME', 'b770ae34b12c19');  
+define('DB_PASSWORD', 'b5c5c63a');  
+define('DB_NAME', 'eu-cdbr-west-02.cleardb.net');  
   
 // Change not required  
 define('PAYPAL_URL', (PAYPAL_SANDBOX == true)?"https://www.sandbox.paypal.com/cgi-bin/webscr":"https://www.paypal.com/cgi-bin/webscr");
