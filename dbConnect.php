0a2b8b4f873ac78c02c9106e4811a5f0215c0756
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-15 17:23:02 +0500

    first commit

--- dbConnect.php
+++ dbConnect.php
@@ -7,8 +7,3 @@ if ($db->connect_errno) {
     printf("Connect failed: %s\n", $db->connect_error);  
     exit();  
 }
-
-define('DB_HOST', 'eu-cdbr-west-02.cleardb.net');  
-define('DB_USERNAME', 'b770ae34b12c19');  
-define('DB_PASSWORD', 'b5c5c63a');  
-define('DB_NAME', 'eu-cdbr-west-02.cleardb.net');  
