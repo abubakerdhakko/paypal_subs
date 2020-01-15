ee2e27ffdc975958ea63caea7dd943b178f00ca8
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-15 18:12:50 +0500

    first commit

--- dbConnect.php
+++ dbConnect.php
@@ -1,10 +1,10 @@
 <?php  
 // Connect with the database  
-$db = new mysqli(eu-cdbr-west-02, b770ae34b12c19, b5c5c63a, heroku_0885a564f4e05ce);  
+$db = new mysqli(eu-cdbr-west-02, b5c5c63a, b770ae34b12c19,  heroku_0885a564f4e05ce);  
   
 // Display error if failed to connect  
 if ($db->connect_errno) {  
     printf("Connect failed: %s\n", $db->connect_error);  
-    
+
     exit();  
 }
