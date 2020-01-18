1c25fad06355a543e648f963d226ebe7594cd326
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-18 23:51:09 +0500

    first commit

--- config.php
+++ config.php
@@ -22,4 +22,4 @@ define('DB_PASSWORD', 'b5c5c63a');
 define('DB_NAME', 'heroku_0885a564f4e05ce');  
   
 // Change not required  
-define('PAYPAL_URL', (PAYPAL_SANDBOX == true)?"https://www.sandbox.paypal.com/cgi-bin/webscr":"https://www.paypal.com/cgi-bin/webscr");
+define('PAYPAL_URL', (PAYPAL_SANDBOX == true)?"https://ipnpb.sandbox.paypal.com/cgi-bin/webscr":"https://ipnpb.sandbox.paypal.com/cgi-bin/webscr");
