8d312d648010598e5789e0beb43584a0bf072dae
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-24 15:22:06 +0500

    test on live

--- dbConnect.php
+++ dbConnect.php
@@ -7,8 +7,8 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL:
 mysql://b770ae34b12c19:b5c5c63a@eu-cdbr-west-02.cleardb.net/heroku_0885a564f4e05ce?reconnect=true "));
 $server = $url["eu-cdbr-west-02.cleardb.net"];
 $username = $url["b770ae34b12c19"];
-$password = $url["b5c5c63a"];
-$db = substr($url["heroku_0885a564f4e05c"], 1);
+$password = $url["b5c5c63aa"];
+$db = substr($url["heroku_0885a564f4e05ce"], 1);
 
 $config = array(
     'host' => $server ,
