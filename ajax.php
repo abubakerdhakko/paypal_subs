85ba0810b31b583c036d12455f937dd0ff104403
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-30 20:38:05 +0500

    add jquery  ajax

--- ajax.php
+++ ajax.php
@@ -8,7 +8,7 @@
 
       $.ajax({ //create an ajax request to display.php
         type: "GET",
-        url: "success.php",
+        url: "test.php",
         dataType: "html", //expect html to be returned                
         success: function(response) {
           $("#responsecontainer").html(response);
