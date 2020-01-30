3f63698b4ddc51047aa148cae4e453d43469f0fc
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-30 20:34:27 +0500

    add jquery  ajax

--- ajax.php
+++ ajax.php
@@ -1,36 +1,35 @@
 <html>
- <script type="text/javascript" src="jquery-1.3.2.js"> </script>
 
- <script type="text/javascript">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
 
- $(document).ready(function() {
+    $("#display").click(function() {
 
-    $("#display").click(function() {                
-
-      $.ajax({    //create an ajax request to display.php
+      $.ajax({ //create an ajax request to display.php
         type: "GET",
-        url: "success.php",             
-        dataType: "html",   //expect html to be returned                
-        success: function(response){                    
-            $("#responsecontainer").html(response); 
-            //alert(response);
+        url: "success.php",
+        dataType: "html", //expect html to be returned                
+        success: function(response) {
+          $("#responsecontainer").html(response);
+          //alert(response);
         }
 
+      });
     });
-});
-});
-
+  });
 </script>
 
 <body>
-<h3 align="center">Manage Student Details</h3>
-<table border="1" align="center">
-   <tr>
-       <td> <input type="button" id="display" value="Display All Data" /> </td>
-   </tr>
-</table>
-<div id="responsecontainer" align="center">
+  <h3 align="center">Manage Student Details</h3>
+  <table border="1" align="center">
+    <tr>
+      <td> <input type="button" id="display" value="Display All Data" /> </td>
+    </tr>
+  </table>
+  <div id="responsecontainer" align="center">
 
-</div>
+  </div>
 </body>
+
 </html>
