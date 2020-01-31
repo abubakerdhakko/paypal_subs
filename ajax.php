9ebf4c18b1811a2ff36c92db400847ef4ef201ae
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-31 17:32:05 +0500

    test ajax

--- ajax.php
+++ ajax.php
@@ -1,11 +1,36 @@
+<?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include_once 'dbConnect.php';
+$result = mysqli_query($db, "SELECT * FROM user_subscriptions");
+
+
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
+
+while ($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['payer_email'] . "</td>";
+  // echo "<td>" . $row['LastName'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($db);
+?>
 <html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript">
   $(document).ready(function() {
     $.ajax({ //create an ajax request to display.php
       type: "GET",
-      url: "test.php",
+      url: "ajax.php",
       dataType: "html", //expect html to be returned                
       success: function(response) {
         $("#responsecontainer").html(response);
