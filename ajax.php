de2e860c5ac7cf23a865771114adea6a39de3492
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-31 17:33:10 +0500

    test ajax

--- ajax.php
+++ ajax.php
@@ -41,14 +41,13 @@ mysqli_close($db);
 </script>
 
 <body>
-  <h3 align="center">Manage Student Details</h3>
+  <h3 align="center">Student Details</h3>
   <table border="1" align="center">
     <tr>
       <td> <input type="button" id="display" value="Display All Data" /> </td>
     </tr>
   </table>
   <div id="responsecontainer" align="center">
-
   </div>
 </body>
 
