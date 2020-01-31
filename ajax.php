cf676b3665b97e2a653e5aef43641993de7f593a
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-01-31 17:53:28 +0500

    test ajax

--- ajax.php
+++ ajax.php
@@ -1,10 +1,36 @@
+<?php
+
+// Include configuration file  
+include_once 'config.php';
+
+// Include database connection file  
+include_once 'dbConnect.php';
+
+if (!empty($_GET['item_number']) && !empty($_GET['tx']) && !empty($_GET['amt']) && $_GET['st'] == 'Completed') {
+  // Get transaction information from URL  
+  $item_number = $_GET['item_number'];
+  $txn_id = $_GET['tx'];
+  $payment_gross = $_GET['amt'];
+  $currency_code = $_GET['cc'];
+  $payment_status = $_GET['st'];
+  $custom = $_GET['cm'];
+
+  // Check if transaction data exists with the same TXN ID.  
+  $prevPaymentResult = $db->query("SELECT * FROM user_subscriptions WHERE txn_id = '" . $txn_id . "'");
+
+  if ($prevPaymentResult->num_rows > 0) {
+    // Get subscription info from database 
+    $paymentData = $prevPaymentResult->fetch_assoc();
+  }
+}
+?>
 <html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript">
   $(document).ready(function() {
     $.ajax({ //create an ajax request to display.php
       type: "GET",
-      url: "success.php",
+      url: "ajax.php",
       dataType: "html", //expect html to be returned                
       success: function(response) {
         $("#responsecontainer").html(response);
