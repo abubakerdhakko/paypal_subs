02efedb9275e7c948b4c2557c63ab967df359e93
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-02-07 17:56:04 +0500

    fix

---
+++ ajax.php
@@ -0,0 +1,34 @@
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+
+    $("#display").click(function() {
+      console.log('fff')
+      $.ajax({ //create an ajax request to display.php
+        type: "GET",
+        url: "test.php",
+        dataType: "html", //expect html to be returned                
+        success: function(response) {
+          $("#responsecontainer").html(response);
+          alert(response);
+        }
+
+      });
+    });
+  });
+</script>
+
+<body>
+  <h3 align="center">Manage Student Details</h3>
+  <table border="1" align="center">
+    <tr>
+      <td> <input type="button" id="display" value="Display All Data" /> </td>
+    </tr>
+  </table>
+  <div id="responsecontainer" align="center">
+
+  </div>
+</body>
+
+</html>
